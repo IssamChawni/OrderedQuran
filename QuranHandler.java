3a90a0b7d237fea05e2d92b104c3308b0562c8f7
IssamChawni <issam.chawni@gmail.com>
2015-07-28 17:35:34 +0200

    Update QuranHandler.java

--- QuranHandler.java
+++ QuranHandler.java
@@ -10,11 +10,11 @@ public class QuranHandler extends DefaultHandler {
 	int rubu = 0;
 	
 	public void startDocument() throws SAXException {
-        System.out.println("Counting letters: \n ----------");
+		System.out.println("Counting letters: \n ----------");
     }
 
     public void endDocument() throws SAXException {
-    	System.out.println(count);
+    	System.out.println("rubu " + rubu + " : " + count);
         System.out.println("------------ \n end Counting.");
     }
 
@@ -27,10 +27,12 @@ public class QuranHandler extends DefaultHandler {
         }
         
         if("hizb".equals(qName) || "rubu".equals(qName) || "nisf".equals(qName)){
-//        	System.out.println("rubu " + rubu + " : " + count);
-        	System.out.println(count);
+        	if( rubu >0){
+        	System.out.println("rubu " + rubu + " : " + count);
         	count = 0;
         	rubu = rubu +1;
+        	}
+        	else { rubu = rubu +1;}
         }
     }
 
